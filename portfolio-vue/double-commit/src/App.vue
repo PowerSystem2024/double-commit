78ea6050022a6d9f9da77ea7887761a418b4f311
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-04 20:06:47 -0300

    remove deffault components from vue...

--- portfolio-vue/double-commit/src/App.vue
+++ portfolio-vue/double-commit/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -9,15 +8,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
