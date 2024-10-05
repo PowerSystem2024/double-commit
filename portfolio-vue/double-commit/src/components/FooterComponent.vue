f11ff5256171d28031dec0c2c3231cfde754baef
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-05 17:02:59 -0300

    se agregaron variables de estilo

---
+++ portfolio-vue/double-commit/src/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<script lang="js"></script>
+
+<template>
+  <footer>
+    <span>&copy;Double-Commit • {{ new Date().getFullYear() }}</span>
+  </footer>
+</template>
+
+<style lang="css" scoped>
+footer {
+  border-top: 1px solid var(--border);
+}
+</style>
