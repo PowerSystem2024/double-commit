26c3eb5b3e6956c4da8bbbfd02f64c0b74571c30
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-05 22:30:32 -0300

    nuevas reglas settings vscode

---
+++ portfolio-vue/double-commit/src/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="js"></script>
+
+<template>
+  <footer>
+    <span>&copy;Double-Commit • {{ new Date().getFullYear() }}</span>
+  </footer>
+</template>
+
+<style lang="css" scoped>
+footer {
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  width: 100%;
+  height: 200px;
+  border-top: 1px solid var(--border);
+}
+</style>
